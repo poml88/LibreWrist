6e89cb11f80e69009df031dfef76f9b1842e324b
Phil <philipp@poeml.de>
2024-07-29 22:13:33 +0200

    Initial Commit

---
+++ LibreWrist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LibreWrist
+//
+//  Created by Peter Müller on 29.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LibreWrist/LibreWristApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreWristApp.swift
+//  LibreWrist
+//
+//  Created by Peter Müller on 29.07.24.
+//
+
+import SwiftUI
+
+@main
+struct LibreWristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
