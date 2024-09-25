b083c6ddee8ea28920f6130c5bd9ffd2e29e3ab3
Phil <philipp@poeml.de>
2024-09-25 15:30:16 +0200

    Update donation views to conform to apple rules.

--- LibreWrist/Views/PhoneAppDonateView.swift
+++ LibreWrist/Views/PhoneAppDonateView.swift
@@ -6,21 +6,60 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct PhoneAppDonateView: View {
+    
+    private let productIDs = [
+        "librewrist_4_99_a",
+        "librewrist_9_99_a",
+        "librewrist_24_99_a",
+        "librewrist_49_99_a"
+    ]
+    
     var body: some View {
-        VStack {
-            Text("If you like this app...\n🙂\n Please Donate! 💪🤝💰")
+        VStack (spacing: 15) {
+            Image("coffeeBeans")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+            Text("If you like this app, buy the developer quality coffee beans!\n🤝☕️")
+                .font(.title3)
                 .multilineTextAlignment(.center)
                 .padding()
             
-            Link(destination: URL(string: "https://paypal.me/lovemyhusky")!) {
-                Text("🐶 PayPal.Me")
-                    .frame(width: 200, height: 50)
-                    .foregroundColor(.primary)
-                    .background(.primary)
-                    .cornerRadius(10)
+            
+            ForEach(productIDs, id: \.self) { id in
+                ProductView(id: id)
+                    .productViewStyle(CustomProductStyle())
             }
+        }
+    }
+}
+
+struct CustomProductStyle: ProductViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        switch configuration.state {
+        case .loading:
+            ProgressView()
+        case .success(let product):
+            Button {
+                configuration.purchase()
+            } label: {
+                VStack {
+                    //                    Text(verbatim: "\(product.displayName) \(product.displayPrice)")
+                    Text(verbatim: product.displayName)
+                    Text(verbatim: product.displayPrice)
+                    //                        .font(.caption2)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            
+            .buttonStyle(.borderedProminent)
+            
+            
+        default:
+            Text("Something went wrong...")
         }
     }
 }
--- LibreWristWatch Watch App/Views/WatchAppDonateView.swift
+++ LibreWristWatch Watch App/Views/WatchAppDonateView.swift
@@ -6,29 +6,73 @@
 //
 
 import SwiftUI
-
+import StoreKit
 
 
 struct WatchAppDonateView: View {
+    
+    private let productIDs = [
+        "librewrist_4_99_a",
+        "librewrist_9_99_a",
+        "librewrist_24_99_a",
+        "librewrist_49_99_a"
+    ]
+    
     var body: some View {
-        VStack {
-            Text("If you like this app...\n🙂\n Please Donate!\n💪🤝💰")
-                .font(.caption)
-                .multilineTextAlignment(.center)
-                .padding()
-            
-            
-                Text("paypal.me/lovemyhusky")
-                .font(.caption)
-                    .frame(maxWidth: 200, maxHeight: 50)
-                    .foregroundColor(.black)
-                    .background(.green)
-                    .cornerRadius(10)
-            
+        ScrollView {
+            VStack {
+                Text("Buy the developer quality coffee beans!\n🤝☕️")
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                
+                //                Text("paypal.me/lovemyhusky")
+                //                .font(.caption)
+                //                    .frame(maxWidth: 200, maxHeight: 50)
+                //                    .foregroundColor(.black)
+                //                    .background(.green)
+                //                    .cornerRadius(10)
+                
+//                StoreView(ids: productIDs)
+//                .productViewStyle(CustomProductStyle())
+//                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                
+                
+                                ForEach(productIDs, id: \.self) { id in
+                                    ProductView(id: id)
+                                        .productViewStyle(CustomProductStyle())
+                                }
+                            
+                
+                
+            }
+            .padding(.top, -20)
         }
     }
 }
 
+struct CustomProductStyle: ProductViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        switch configuration.state {
+        case .loading:
+            ProgressView()
+        case .success(let product):
+            Button {
+                configuration.purchase()
+            } label: {
+                VStack {
+                    Text(verbatim: "\(product.displayName) \(product.displayPrice)")
+                        .font(.caption2)
+                }
+            }
+            .buttonStyle(.borderedProminent)
+        default:
+            Text("Something went wrong...")
+        }
+    }
+}
+    
 #Preview {
     WatchAppDonateView()
 }
