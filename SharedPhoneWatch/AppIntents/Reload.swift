36845669609ba4523a66a480562b2fb111987d18
Phil <philipp@poeml.de>
2024-10-10 23:58:03 +0200

    void

--- SharedPhoneWatch/AppIntents/Reload.swift
+++ SharedPhoneWatch/AppIntents/Reload.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
+//  Reload.swift
 //  LibreWrist
 //
 //  Created by Peter Müller on 10.10.24.
 //
 
-import Foundation
+
+
+import AppIntents
+
+struct ReloadWidgetIntent: AppIntent {
+    static var title: LocalizedStringResource = "Reload widget"
+    static var description = IntentDescription("Reload widget.")
+
+    init() {}
+
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
